66b5671eb8023a2a69240a5ed940f91abbb53e90
Sarwang Jain <jainsarwang@gmail.com>
2025-03-22 01:07:40 +0530

    exported latest mysql structure

--- attendance_system.sql
+++ attendance_system.sql
@@ -1,93 +1,290 @@
-CREATE TABLE `users` (
- `id` varchar(255) NOT NULL,
- `name` varchar(255) NOT NULL,
- `mobile` bigint(20) NOT NULL,
- `user` varchar(255) NOT NULL,
- `password` varchar(255) NOT NULL,
- `type` enum('permanent','guest') NOT NULL,
- `role` enum('hod','faculty','staff') NOT NULL,
- PRIMARY KEY (`id`),
- UNIQUE KEY `PRIMARY2` (`user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 01, 2025 at 12:08 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
-CREATE TABLE `classes` (
- `id` varchar(255) NOT NULL,
- `batch` int(11) NOT NULL,
- `department` varchar(255) NOT NULL,
- `semester` int(11) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-CREATE TABLE `students` (
- `enrollment_number` varchar(255) NOT NULL,
- `name` varchar(255) NOT NULL,
- `mobile` bigint(20) NOT NULL,
- `gender` enum('M','F','O') NOT NULL,
- `class_id` varchar(255) DEFAULT NULL,
- PRIMARY KEY (`enrollment_number`),
- KEY `FOREIGN_CLASS` (`class_id`),
- CONSTRAINT `FOREIGN_CLASS` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE `subject` (
- `subject_code` varchar(30) NOT NULL,
- `name` varchar(255) NOT NULL,
- PRIMARY KEY (`subject_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `teaches` (
- `id` varchar(255) NOT NULL,
- `class_id` varchar(255) NOT NULL,
- `subject_id` varchar(255) NOT NULL,
- `teacher_id` varchar(255) NOT NULL,
- PRIMARY KEY (`id`),
- KEY `FOREIGN_SUBJECT` (`subject_id`),
- KEY `FOREIGN_TEACHER` (`teacher_id`),
- KEY `FOREIGN_CLASS_TEACHES` (`class_id`),
- CONSTRAINT `FOREIGN_CLASS_TEACHES` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
- CONSTRAINT `FOREIGN_SUBJECT` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_code`),
- CONSTRAINT `FOREIGN_TEACHER` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Database: `attendance_system`
+--
 
-CREATE TABLE `permissions` (
- `id` int(11) NOT NULL,
- `name` varchar(255) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `attendance_system`;
 
-CREATE TABLE `role_has_permission` (
- `id` int(11) NOT NULL,
- `role` enum('hod','faculty','staff') NOT NULL,
- `permission` int(11) NOT NULL,
- PRIMARY KEY (`id`),
- KEY `FOREIGN_PERMISSION` (`permission`),
- CONSTRAINT `FOREIGN_PERMISSION` FOREIGN KEY (`permission`) REFERENCES `permissions` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+use `attendance_system`;
 
-CREATE TABLE `attendance_record` (
- `id` varchar(255) NOT NULL,
- `teaches_id` varchar(255) NOT NULL,
- `class_type` enum('THEORY','PRACTICAL') NOT NULL,
- `time` bigint(20) NOT NULL,
- `created_time` bigint(20) NOT NULL,
- `updated_time` bigint(20) NOT NULL,
- PRIMARY KEY (`id`),
- KEY `FOREIGN_TEACHES` (`teaches_id`),
- CONSTRAINT `FOREIGN_TEACHES` FOREIGN KEY (`teaches_id`) REFERENCES `teaches` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendances`
+--
 
 CREATE TABLE `attendances` (
- `id` varchar(255) NOT NULL,
- `student_id` varchar(255) NOT NULL,
- `record_id` varchar(255) NOT NULL,
- `status` enum('P','A') NOT NULL,
- PRIMARY KEY (`id`),
- KEY `FOREIGN_STUDENT` (`student_id`),
- KEY `FOREIGN_RECORD` (`record_id`),
- CONSTRAINT `FOREIGN_RECORD` FOREIGN KEY (`record_id`) REFERENCES `attendance_record` (`id`),
- CONSTRAINT `FOREIGN_STUDENT` FOREIGN KEY (`student_id`) REFERENCES `students` (`enrollment_number`)
+  `id` varchar(255) NOT NULL,
+  `student_id` varchar(255) NOT NULL,
+  `record_id` varchar(255) NOT NULL,
+  `status` enum('P','A') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `attendance_record`
+--
 
+CREATE TABLE `attendance_record` (
+  `id` varchar(255) NOT NULL,
+  `teaches_id` varchar(255) NOT NULL,
+  `class_type` enum('THEORY','PRACTICAL') NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `created_time` bigint(20) NOT NULL,
+  `updated_time` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `sem_start` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_has_permission`
+--
+
+CREATE TABLE `role_has_permission` (
+  `id` int(11) NOT NULL,
+  `role` enum('hod','faculty','staff') NOT NULL,
+  `permission` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `role_permission_view`
+-- (See below for the actual view)
+--
+CREATE TABLE `role_permission_view` (
+`role` enum('hod','faculty','staff')
+,`permissions` varchar(255)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `enrollment_number` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `gender` enum('M','F','O') NOT NULL,
+  `class_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_code` varchar(30) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teaches`
+--
+
+CREATE TABLE `teaches` (
+  `id` varchar(255) NOT NULL,
+  `class_id` varchar(255) NOT NULL,
+  `subject_id` varchar(255) NOT NULL,
+  `teacher_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` enum('permanent','guest') NOT NULL,
+  `role` enum('hod','faculty','staff') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `role_permission_view`
+--
+DROP TABLE IF EXISTS `role_permission_view`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `role_permission_view`  AS SELECT `r`.`role` AS `role`, `p`.`name` AS `permissions` FROM (`role_has_permission` `r` join `permissions` `p` on(`r`.`permission` = `p`.`id`))  ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FOREIGN_STUDENT` (`student_id`),
+  ADD KEY `FOREIGN_RECORD` (`record_id`);
+
+--
+-- Indexes for table `attendance_record`
+--
+ALTER TABLE `attendance_record`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FOREIGN_TEACHES` (`teaches_id`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `role_has_permission`
+--
+ALTER TABLE `role_has_permission`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FOREIGN_PERMISSION` (`permission`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`enrollment_number`),
+  ADD KEY `FOREIGN_CLASS` (`class_id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_code`);
+
+--
+-- Indexes for table `teaches`
+--
+ALTER TABLE `teaches`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FOREIGN_SUBJECT` (`subject_id`),
+  ADD KEY `FOREIGN_TEACHER` (`teacher_id`),
+  ADD KEY `FOREIGN_CLASS_TEACHES` (`class_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `PRIMARY2` (`user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `role_has_permission`
+--
+ALTER TABLE `role_has_permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD CONSTRAINT `FOREIGN_RECORD` FOREIGN KEY (`record_id`) REFERENCES `attendance_record` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FOREIGN_STUDENT` FOREIGN KEY (`student_id`) REFERENCES `students` (`enrollment_number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `attendance_record`
+--
+ALTER TABLE `attendance_record`
+  ADD CONSTRAINT `FOREIGN_TEACHES` FOREIGN KEY (`teaches_id`) REFERENCES `teaches` (`id`);
+
+--
+-- Constraints for table `role_has_permission`
+--
+ALTER TABLE `role_has_permission`
+  ADD CONSTRAINT `FOREIGN_PERMISSION` FOREIGN KEY (`permission`) REFERENCES `permissions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `FOREIGN_CLASS` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`);
+
+--
+-- Constraints for table `teaches`
+--
+ALTER TABLE `teaches`
+  ADD CONSTRAINT `FOREIGN_CLASS_TEACHES` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
+  ADD CONSTRAINT `FOREIGN_SUBJECT` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_code`),
+  ADD CONSTRAINT `FOREIGN_TEACHER` FOREIGN KEY (`teacher_id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- inc/header.php
+++ inc/header.php
@@ -1,14 +1,14 @@
-
 <header>
     <!-- <div class="container"> -->
-        <div id="site_logo">
-            <h2>Attendance System</h2>
-        </div>
+    <div id="site_logo">
+        <h2><?= APP_NAME ?></h2>
+    </div>
 
-        <div id="header_user" title="<?= $userData['user'] ?>">
-                <img src="https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg" alt="User LOGO">
+    <div id="header_user" title="<?= $userData['user'] ?>">
+        <img src="https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg"
+            alt="User LOGO">
 
-            <span><?= $userData['name'] ?></span>
-        </div>
+        <span><?= $userData['name'] ?></span>
+    </div>
     <!-- </div> -->
 </header>
